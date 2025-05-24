8a3c115935c5dc8503db1a77e065158f01253cf0
Rahul Mishra <rahulamishra1305@gmail.com>
2025-05-24 21:42:59 +0530

    new

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,16 +1,48 @@
+// import mongoose from "mongoose";
+
+// const connectDb = async () => {
+//         try {
+//             const conn = await mongoose.connect(process.env.MONGO_URI, {
+//                 useNewUrlParser: true,
+//             });
+//             console.log(`MongoDB Connected: ${conn.connection.host}`);
+//             return conn;
+
+//         } catch (error) {
+//             console.error(error.message);
+//             process.exit(1);
+//         }
+//     }
+
+//   export default connectDb;
+
+// /lib/connectDb.js or /db/connectDb.js
+
 import mongoose from "mongoose";
 
-const connectDb = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
+const MONGODB_URI = process.env.MONGO_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGO_URI not defined in environment variables");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDb() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-    return conn;
-  } catch (error) {
-    console.error(error.message);
-    process.exit(1);
   }
-};
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 export default connectDb;
